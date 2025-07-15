757c8425db38137ecd77b32a58f6a667d00a30cb
coolBill223 <bzhou6198@gmail.com>
2025-07-14 21:59:35 -0400

    Initialize the app

---
+++ my-app/backend/firebaseConfig.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCOZWRdN_MWp6QQwX2qlNLlpTBmPibh7VA",
+  authDomain: "cs4720-d84f6.firebaseapp.com",
+  projectId: "cs4720-d84f6",
+  storageBucket: "cs4720-d84f6.firebasestorage.app",
+  messagingSenderId: "300772083431",
+  appId: "1:300772083431:web:31002d8cec47dcb8900442",
+  measurementId: "G-LVWQT91RJJ"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
