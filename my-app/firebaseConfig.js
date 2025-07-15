91d237b5f2b26023e52e99886c0ffc6261abdc81
coolBill223 <bzhou6198@gmail.com>
2025-07-15 13:19:39 -0400

    fix bugs for not login

--- my-app/firebaseConfig.js
+++ my-app/firebaseConfig.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-import { getAnalytics } from "firebase/analytics";
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyCOZWRdN_MWp6QQwX2qlNLlpTBmPibh7VA",
@@ -15,7 +15,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
